b77da0d138f64b85382d641d8475c5eaea452100
Dmitry <Ivanovdmitry530@gmail.com>
2023-07-19 11:00:33 +0400

    Добавьте файлы проекта.

---
+++ PrivateCritter/PrivateCritter.cpp
@@ -0,0 +1,53 @@
+﻿// PrivateCritter
+// Деномстрирует установку уровней доступа к членам
+
+#include <iostream>
+using namespace std;
+
+class Critter
+{
+    public: // начало открытого раздела
+        Critter(int hunger = 0);
+        int GetHunger() const;
+        void SetHunger(int hunger);
+    private: // начало приватного раздела
+        int m_Hunger;
+};
+
+Critter::Critter(int hunger):
+m_Hunger(hunger)
+{
+    cout << "A new critter has been born!" << endl;
+}
+
+int Critter::GetHunger() const
+{
+    return m_Hunger;
+}
+
+void Critter::SetHunger(int hunger)
+{
+    if (hunger < 0)
+    {
+        cout << "You can't set a critter's hunger to a negative number.\n\n";
+    }
+    else
+    {
+        m_Hunger = hunger;
+    }
+}
+
+int main()
+{
+    Critter crit(5);
+    // cout <<crit.m_Hunger; // недопустимо, так как член Hunger является закрытым
+    cout << "Calling GetHunger(): " << crit.GetHunger() << "\n\n";
+    cout << "Calling SetHunger() with -1.\n";
+    crit.SetHunger(-1);
+    cout << "Calling SetHunger() with 9.\n";
+    crit.SetHunger(9);
+    cout << "Calling GetHunger():" << crit.GetHunger() << "\n\n";
+    return 0;
+}
+
+
